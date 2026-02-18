9d2fc018cfd6e20cb96123adf7bddaad989c331f
Bella Rose Wilson <bwilson9893@gmail.com>
2026-02-18 03:29:18 +0000

    -removed secret to save/ pause at first image test

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,5 +1,5 @@
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV['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'] || Rails.application.credentials.dig(:openai_api_key)
+  config.openai_api_key = ENV['OPENAI_API_KEY'] || Rails.application.credentials.dig(:openai_api_key)
   # config.default_model = "gpt-4.1-nano"
 
   # Use the new association-based acts_as API (recommended)
